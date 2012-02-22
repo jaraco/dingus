2b69216d1612010fde446027f57825f52b181493
Jason R. Coombs <jaraco@jaraco.com>
2012-02-22 08:52:33 -0500

    Added a test to demonstrate usage of custom test base

--- tests/test_dingus_test_case.py
+++ tests/test_dingus_test_case.py
@@ -4,6 +4,7 @@ from tests import test_case_fixture as module
 from tests.test_case_fixture import ClassUnderTest, Collaborator
 
 from dingus import DingusTestCase, Dingus
+import dingus
 
 
 class WhenObjectIsExcludedFromTest:
@@ -66,3 +67,27 @@ class WhenCallingTeardownFunction:
     def should_leave_globals_as_they_were_before_dingusing(self):
         assert module.__dict__ == self.original_module_dict
 
+
+class WhenCustomTestCaseBase:
+    def setup(self):
+        class CustomTestBase(dingus.TestCase):
+            """
+            This TestBase enables setup/teardown in py.test tests
+            """
+            def setup_method(self, method):
+                self.setup()
+
+            def teardown_method(self, method):
+                self.teardown()
+
+        class TestCase(DingusTestCase(module.ClassUnderTest,
+                base=CustomTestBase)):
+            pass
+        self.test_case_instance = TestCase()
+        self.test_case_instance.setup()
+
+    def teardown(self):
+        self.test_case_instance.teardown()
+
+    def test_case_should_have_setup_method_method(self):
+        assert hasattr(self.test_case_instance, 'setup_method')
